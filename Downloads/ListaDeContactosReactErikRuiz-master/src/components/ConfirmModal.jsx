6b23858b9d26df27be575e660bf5d65106c88b91
Samuel Diaz <90120898+samuelgiorno@users.noreply.github.com>
2025-05-17 19:24:45 +0200

    Delete Downloads directory

--- Downloads/ListaDeContactosReactErikRuiz-master/src/components/ConfirmModal.jsx
+++
@@ -1,7 +0,0 @@
-<ConfirmModal
-  show={showModal}
-  onCancel={() => setShowModal(false)}
-  onConfirm={handleDelete}
-  title="Confirm delete"
-  message="Are you sure you want to delete this contact?"
-/>
--- Downloads/ListaDeContactosReactErikRuiz-master/src/components/Spinner.jsx
+++
@@ -1,8 +0,0 @@
-const LoadingSpinner = () => (
-    <div className="text-center my-4">
-      <div className="spinner-border text-primary" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </div>
-    </div>
-  );
-  
